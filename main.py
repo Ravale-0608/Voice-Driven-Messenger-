e82bda95124ca6e3df2f211acfb4f4d3389bd7c6
Eligible_programmer <ravalekhan007@gmail.com>
2022-07-16 14:43:28 -0400

    Alfred can send messages via voice recognition now.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import time
 import pyaudio
+import datetime
 import speech_recognition as sr
 from gtts import gTTS
 import subprocess
+import pywhatkit
+now = datetime.datetime.now()
 
 
 def speak(text):
@@ -18,13 +21,27 @@ def get_audio():
         audio = r.listen(source)
         said = ""
 
-        while said != "bye":
-            try:
-                said = r.recognize_google(audio)
-                speak(said)
-            except Exception as e:
-                print("Exception: " + str(e))
+        try:
+            said = r.recognize_google(audio)
+        except Exception as e:
+            print("Exception: " + str(e))
     return said
 
 
-get_audio()
+contacts = {'brother': '+15714885819', 'amna': '+15716396640', 'mom': '+15878909324', 'dad': '+923008466642'
+            }
+speak("Nice to meet you. My name is Alfred!")
+text = get_audio()
+if 'message' in text:
+    speak("Who do you want to send a message to?")
+    text = get_audio()
+    speak("What is your message?")
+    msg = get_audio()
+    for contact in contacts:
+        if contact in text:
+            speak("Messaging "+str(contact))
+            num = contacts[contact]
+            hour = now.hour
+            minute = now.minute
+            pywhatkit.sendwhatmsg(num, msg, hour, minute+2)
+            speak("Message successfully sent")
